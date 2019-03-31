7627e68e2707e019b7bccb0413d126610df688ab
Osiel Lima Díaz <osielmesa@gmail.com>
2019-03-31 11:02:13 +0200

    Initial commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href={item.link} target="_blank">
+                      <img src={`${item.imgurl}`} className="item-img" style={{maxHeight:300}} />
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+          <h1>Check Out Some of My Works As Contributor.</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            {
+              resumeData.portfolio_contributors && resumeData.portfolio_contributors.map((item)=>{
+                return(
+                  <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                      <a href={item.link} target="_blank">
+                        <img src={`${item.imgurl}`} className="item-img" style={{maxHeight:200}}/>
+                        <div className="overlay">
+                          <div className="portfolio-item-meta">
+                            <h5>{item.name}</h5>
+                            <p>{item.description}</p>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,162 @@
+let code = `
+      if(footing || skate || fishing || hangOutWithFriends){
+        this.goOutInMyFreeTime()
+      }
+    `
+
+let resumeData = {
+    "name": "Osiel Lima Díaz",
+    "role": "Software Developer",
+    "code": code,
+    "email":"osielmesa@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like frontend development and to learn about new technologies, read technical articles or simply:",
+    "socialLinks":[
+        {
+          "name":"Linkedin",
+          "url":"https://www.linkedin.com/in/osiel/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"skype",
+          "url":"https://join.skype.com/invite/wzX9qDDT2bcO",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"I’m a software developer with 7 years of experience in both web and mobile apps. I have specialized in front-end development especially in React Native, android and iOS (swift) but I have also worked with React and Firebase (back-end), currently diving deeper into ARCore (android) from Google, Amazon Web Services and aws-amplify.\n" +
+      "\n" +
+      "Recently, I publish a small android (ARCore) app using an AR camera library built by myself. In this role, I was focused on decreasing consumption time of resources and creating a great user experience.\n" +
+      "\n" +
+      "Colleagues know me as a highly creative developer who can always be trusted to propose a new approach. But I know that user experience and good performance comes first, so I never try to impose my ideas on others. I can (and often do) work well alone, but I’m at my best collaborating with others.",
+    "address":"Serbia",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Universidad de las Ciencias Informáticas",
+        "link":"https://www.uci.cu/",
+        "specialization":"Bachelor's degree in Computer Science",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2008 - 2013",
+        "Achievements":"University of Informatic Sciences. Software development process, certified with CMMI Level II.\n" +
+          "Engineering in Computer Science and Master in Management of Computer Projects, certified by the National Accreditation Board. Engineers in Computer Science with a high level of specialization in the work with the Information and communication technologies."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Universidad de las Ciencias Informáticas",
+        "specialization":"Software developer",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2016",
+        "country":"Cuba",
+        "city":"Havana",
+        "Achievements":"Cuba. Part of my job was when I was still a student, based on Android. After graduating, I was involved in web development using Symfony framework, javascript, html and css."
+      },
+      {
+        "CompanyName":"Multiversoft SL",
+        "specialization":"Senior Software Engineer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "country":"Cuba",
+        "city":"Holguin",
+        "Achievements":"I was involved on development of a real time platform in the cloud using Swift, Android, Java, Kubernetes, Google Cloud Platform, PostgreSQL and RabbitMQ, mainly focused on iOS app development using Swift."
+      },
+      {
+        "CompanyName":"Multiversoft SL",
+        "specialization":"Full Stack Software Engineer",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2019",
+        "country":"Serbia",
+        "city":"Belgrade",
+        "Achievements":"Serbia. After my relocation to Belgrade, Serbia I focused on the development of a real-time platform in the cloud, acquiring experience in the development of mobile platforms with React Native, React, Redux and Firebase. \n" +
+          "\n" +
+          "Currently, I am developing a new version of the platform with React Native, React, Redux and Firebase. I'm in charge of maintaining the previous version of the platform regarding mobile applications using iOS (Swift) and Android."
+      }
+    ],
+    "skillsDescription":"Main skills",
+    "skills":[
+      {
+        "skillname":"React Native",
+        "percent": "90%"
+      },
+      {
+        "skillname":"React",
+        "percent": "80%"
+      },
+      {
+        "skillname":"Firebase",
+        "percent": "70%"
+      },
+      {
+        "skillname":"Swift",
+        "percent": "75%"
+      },
+      {
+        "skillname":"Android",
+        "percent": "75%"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Votephone",
+        "description":"iOS app.",
+        "imgurl":"https://lh3.googleusercontent.com/O-4UAWOX-zy6-wyufRWWHGTU-Shdcrw9O3VvNpL3QCq4D6eYEpEweqF4cbcIlLz6WqOP=s360-rw",
+        "link":"https://itunes.apple.com/es/app/votephone/id1148501590?l=en&mt=8"
+      },
+      {
+        "name":"Extractory - Augmented Reality",
+        "description":"Android app (ARCore and Sceneform).",
+        "imgurl":"https://lh3.googleusercontent.com/-nxneqGDD2iYZHC8qoRKtu8SIJWgvRcJlgVeEkXnFbnj5zi7wZjDgcJKZQuLA1ppnV8=s360-rw",
+        "link":"https://play.google.com/store/apps/details?id=com.freebird.extractory"
+      },
+      {
+        "name":"Votephone",
+        "description":"React Native app (Newer version of android and iOS app but with social network approach). *It is not a public repo. It is not in production yet.",
+        "imgurl":"images/portfolio/votephone_rn.png",
+        "link":"#"
+      },
+      {
+        "name":"TeamResults",
+        "description":"React Native app (only for management). *It is not a public repo. It is in production but not in PlayStore.",
+        "imgurl":"images/portfolio/team_results.png",
+        "link":"#"
+      },
+      {
+        "name":"Crypto search",
+        "description":"React Native app made in 5 days for a coding test. This is an example project.",
+        "imgurl":"https://raw.githubusercontent.com/osielmesa/cryptosearch/master/screenshots/welcome.png",
+        "link":"https://github.com/osielmesa/cryptosearch"
+      },
+      {
+        "name":"Hotel finder",
+        "description":"React Native app made in approximately 8 hours for a coding test. This is an example project. It works but only with a docker image as backend provided by judges of the test at the moment.",
+        "imgurl":"images/portfolio/origami.jpg",
+        "link":"https://github.com/osielmesa/hotelfinder"
+      }
+    ],
+  "portfolio_contributors":[
+    {
+      "name":"Votephone",
+      "description":"Android app",
+      "imgurl":"https://lh3.googleusercontent.com/O-4UAWOX-zy6-wyufRWWHGTU-Shdcrw9O3VvNpL3QCq4D6eYEpEweqF4cbcIlLz6WqOP=s360-rw",
+      "link":"https://play.google.com/store/apps/details?id=com.multiversoft.votephone&hl=en_US"
+    },
+    {
+      "name":"Yuge",
+      "description":"Android app",
+      "imgurl":"https://lh3.googleusercontent.com/M-E_443eFToRizkn6hhFOTJ_bc-HN9nj22cOKJV2jKGT2v3Y8Qr_CDafsmeYMAxtG0c=s360-rw",
+      "link":"https://play.google.com/store/apps/details?id=com.goyuge.android"
+    },
+  ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+
+  export default resumeData
