9c8b0dbc5be201a9f1c2df957c9342fc848db257
Osiel Lima Díaz <osielmesa@gmail.com>
2021-08-01 17:05:12 +0200

    Just a testing script tag file

---
+++ productARStriptTag.js
@@ -0,0 +1,3 @@
+console.log('osiel this is the sript tag');
+var div = document.getElementById('ScriptApp');
+div.innerHTML = '<div style="background-color:green;text-align:center;color:black;"><p>osiel el loco</p></div>'
